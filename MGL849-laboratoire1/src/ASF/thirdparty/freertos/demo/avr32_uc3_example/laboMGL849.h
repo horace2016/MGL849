9fa8770644b24baeb93a86cc93e0d75ae8689f69
Scentle5S <nicolas.bfd@gmail.com>
2017-01-31 17:59:48 -0500

    Initial commit

---
+++ MGL849-laboratoire1/src/ASF/thirdparty/freertos/demo/avr32_uc3_example/laboMGL849.h
@@ -0,0 +1,98 @@
+
+
+/* Connection of the temperature sensor */
+#  define ADC_TEMPERATURE_CHANNEL     0
+#  define ADC_TEMPERATURE_PIN         AVR32_ADC_AD_0_PIN
+#  define ADC_TEMPERATURE_FUNCTION    AVR32_ADC_AD_0_FUNCTION
+
+/* Connection of the potentiometer */
+#  define ADC_POTENTIOMETER_CHANNEL   1
+#  define ADC_POTENTIOMETER_PIN       AVR32_ADC_AD_1_PIN
+#  define ADC_POTENTIOMETER_FUNCTION  AVR32_ADC_AD_1_FUNCTION
+
+
+void initializeLCD(void);
+void initializeADC(void);
+
+/************************************************************************************/
+
+
+volatile signed short adc_value_pot   = -1;
+volatile signed short adc_value_temp  = -1;
+
+void initializeLCD(void){
+	
+	  static const gpio_map_t DIP204_SPI_GPIO_MAP =
+	  {
+		  {DIP204_SPI_SCK_PIN,  DIP204_SPI_SCK_FUNCTION },  // SPI Clock.
+		  {DIP204_SPI_MISO_PIN, DIP204_SPI_MISO_FUNCTION},  // MISO.
+		  {DIP204_SPI_MOSI_PIN, DIP204_SPI_MOSI_FUNCTION},  // MOSI.
+		  {DIP204_SPI_NPCS_PIN, DIP204_SPI_NPCS_FUNCTION}   // Chip Select NPCS.
+	  };
+
+	  // Disable all interrupts
+	  Disable_global_interrupt();
+
+	  Enable_global_interrupt();
+
+	  // add the spi options driver structure for the LCD DIP204
+	  spi_options_t spiOptions =
+	  {
+		  .reg	= DIP204_SPI_NPCS,
+		  .baudrate	= 1000000,
+		  .bits		= 8,
+		  .spck_delay	= 0,
+		  .stay_act	= 1,
+		  .trans_delay = 8,
+		  .spi_mode	= 0,
+		  .modfdis	= 1
+	  };
+	  // Assign I/Os to SPI
+	  gpio_enable_module(DIP204_SPI_GPIO_MAP, sizeof(DIP204_SPI_GPIO_MAP)/sizeof(DIP204_SPI_GPIO_MAP[0]));
+
+	  // Initialize as master
+	  spi_initMaster(DIP204_SPI, &spiOptions);
+
+	  // Set selection mode: variable_ps, pcs_decode, delay
+	  spi_selectionMode(DIP204_SPI, 0, 0, 0);
+
+	  //Enable SPI
+	  spi_enable(DIP204_SPI);
+
+	  // setup chip registers
+	  spi_setupChipReg(DIP204_SPI, &spiOptions, FOSC0);
+
+	  // initialize delay driver
+	  delay_init( FOSC0 );
+	  
+	  // initialize LCD
+	  dip204_init(backlight_PWM, true);
+}
+
+
+void initializeADC(void){
+
+	/** GPIO pin/adc-function map. */
+	const gpio_map_t ADC_GPIO_MAP = {
+		{ADC_TEMPERATURE_PIN, ADC_TEMPERATURE_FUNCTION},
+		{ADC_POTENTIOMETER_PIN, ADC_POTENTIOMETER_FUNCTION}
+	};
+
+	/* Init system clocks */
+	sysclk_init();
+
+	/* Assign and enable GPIO pins to the ADC function. */
+	gpio_enable_module(ADC_GPIO_MAP, sizeof(ADC_GPIO_MAP) /
+	sizeof(ADC_GPIO_MAP[0]));
+	
+	/* Configure the ADC peripheral module.
+	 * Lower the ADC clock to match the ADC characteristics (because we
+	 * configured the CPU clock to 12MHz, and the ADC clock characteristics are
+	 *  usually lower; cf. the ADC Characteristic section in the datasheet). */
+	AVR32_ADC.mr |= 0x1 << AVR32_ADC_MR_PRESCAL_OFFSET;
+	adc_configure(&AVR32_ADC);
+	
+	/* Enable the ADC channels. */
+	adc_enable(&AVR32_ADC, ADC_TEMPERATURE_CHANNEL);
+	adc_enable(&AVR32_ADC, ADC_POTENTIOMETER_CHANNEL);
+}
---
+++ MGL849-laboratoire1/src/ASF/thirdparty/freertos/demo/avr32_uc3_example/main.c
@@ -0,0 +1,57 @@
+
+/************************************************************************/
+/* MGL849 Laboratoire 1 squelette                                       */
+/* Noms et Code permanent des étudiants:                                */
+/*  Mon Nom - codepermanent                                             */
+/************************************************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/* Environment header files. */
+#include "power_clocks_lib.h"
+
+/* Scheduler header files. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+//#include "asf.h"  // horace
+#include <asf.h>
+#include "laboMGL849.h"
+#include "myAllTask.h"
+
+
+int main( void )
+{
+
+	// Configure Osc0 in crystal mode (i.e. use of an external crystal source, with
+	// frequency FOSC0 (12Mhz) ) with an appropriate startup time then switch the main clock
+	// source to Osc0.
+	//pcl_switch_to_osc(PCL_OSC0, FOSC0, OSC0_STARTUP);
+	pm_switch_to_osc0(&AVR32_PM, FOSC0, OSC0_STARTUP);
+	
+	/* Initialisation de l'ADC*/
+	initializeADC();
+	
+	/* Initialisation du LCD*/	
+	initializeLCD();
+	
+	// Initialize your semaphore
+	SYNCHRO_SEMAPHORE  =  xSemaphoreCreateMutex ( );
+
+	if(!( pdPASS == xTaskCreate( vTaskReadADC,    ( signed char * ) "Lecture ADC",    200,  NULL, READ_ADC_PRIORITY,  (xTaskHandle*)  id_vTaskReadADC    ))) goto hell;   
+	if(!( pdPASS == xTaskCreate( vTaskRefreshLCD, ( signed char * ) "Task afficage",  5000, NULL, LCD_REFRESH_PRIORITY, (xTaskHandle*) id_vTaskRefreshLCD ))) goto hell;
+	if(!( pdPASS == xTaskCreate( vTaskLedPOWER,   ( signed char * ) "Task LED",        2000,  NULL, TRAITEMENT_PRIORITY,  (xTaskHandle*) id_vTaskLedPOWER )))  goto hell;
+		
+	/* Start the scheduler. */
+	vTaskStartScheduler();
+
+	hell:
+	/* Will only get here if there was insufficient memory to create the idle
+	task. */
+	return 0;
+}
